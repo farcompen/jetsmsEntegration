f78daf6af9e2a8f58485ad0ea55229d228f708b4
Faruk GÜNGÖR <farcompen71@gmail.com>
2017-05-02 16:23:57 +0300

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,83 @@
+﻿namespace dakik_sms
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(181, 239);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(181, 56);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(247, 96);
+            this.richTextBox1.TabIndex = 1;
+            this.richTextBox1.Text = "";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(181, 181);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 2;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(687, 529);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.TextBox textBox1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace dakik_sms
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            mesaj_gonder();
+        }
+
+        public void mesaj_gonder()
+        {
+            servis_sms.SendSMSResult sonuc = new servis_sms.SendSMSResult();
+            string kullanici = "kamuhas";
+            string parola = "1659km";
+            string originator = "DEMO";
+            string reference = "ben";
+            string bas_tarih = "14042017100000";
+            string bitis = "15052017100000";
+            servis_sms.ArrayOfString mesaj = new servis_sms.ArrayOfString();
+            mesaj.Add(richTextBox1.Text);
+            servis_sms.ArrayOfString numaralar = new servis_sms.ArrayOfString();
+            numaralar.Add(textBox1.Text);
+            string channel = null;
+            string karaliste = null;
+            string option_f = null;
+            string x = null;
+            servis_sms.enmOnLengthProblem onn = new servis_sms.enmOnLengthProblem();
+
+
+            string yasak_saat_bas = "230000";
+            string yasak_saat_bitis = "080000";
+
+            servis_sms.SMSServiceSoapClient client = new servis_sms.SMSServiceSoapClient();
+            sonuc = client.SendSMS(kullanici, parola, originator, reference, bas_tarih, bitis, mesaj, numaralar, yasak_saat_bas, yasak_saat_bitis, channel, karaliste, option_f, x, onn);
+
+            if (sonuc.ErrorCode == "00")
+                MessageBox.Show("Başarılı");
+            else
+                MessageBox.Show(sonuc.ErrorCode);
+                
+
+        }
+
+       
+    }
+}
